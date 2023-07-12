827de200ddcca0013ed50643d3295fb90946721e
victormc13 <viktorsb14@gmail.com>
2023-07-11 23:12:41 -0500

    Set up linters files configuration

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import _ from 'lodash';
 import './style.css';
 
 const tasks = [
   {
-    description: "Buy some food",
+    description: 'Buy some food',
     completed: false,
     index: 1,
   },
   {
-    description: "Go for walk",
+    description: 'Go for walk',
     completed: false,
     index: 2,
   },
   {
-    description: "Do laundry",
+    description: 'Do laundry',
     completed: false,
     index: 3,
   },
 ];
 
 const renderTasks = () => {
-  const taskContainer = document.querySelector(".task-container");
-  taskContainer.innerHTML = "";
+  const taskContainer = document.querySelector('.task-container');
+  taskContainer.innerHTML = '';
 
   tasks.sort((a, b) => a.index - b.index);
 
@@ -36,8 +35,8 @@ const renderTasks = () => {
 
     taskContainer.insertAdjacentHTML('beforeend', taskElement);
   });
-}
+};
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   renderTasks();
 });
