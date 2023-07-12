3ba8328c0ce1dcb346729d0b41268c11875ac129
victormc13 <viktorsb14@gmail.com>
2023-07-12 11:19:52 -0500

    Remove all hardcoded items from the tasks array

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Buy some food',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Go for walk',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Do laundry',
-    completed: false,
-    index: 3,
-  },
-];
+const tasks = [];
 
 const renderTasks = () => {
   const taskContainer = document.querySelector('.task-container');
